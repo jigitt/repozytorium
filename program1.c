a5627fc5bb874914c026f47257ea19d012fc187c
jigitt <tomasz.siemianowski@gmail.com>
2017-10-20 14:05:08 +0200

    zmiana2

--- program1.c
+++ program1.c
@@ -1,13 +1,33 @@
-#include <stdio.h> /* biblioteka standardowa input-output */
+#include <stdio.h>
 
-/* y= f(x) */
-/* f() => 0 */
-
-int main() /*funkcja główna programu */
-/* int -> typ zwracanej wartości (int - wartosc calkowita)*/
-
+int mnoz(int a, int b) /* funkcja mnozaca */
 {
-        puts("Pierwszy program"); /*puts - funkcja z biblioteki standardowej */
-                                /* wyswietlajaca ciag znakow */
+return a*b;
+}
+
+int dodaj(int a, int b) /*funkcja dodajaca 2 liczby */
+{
+return a+b;
+}
+
+int main(void)
+{
+        int a = 2, b = 5;  /* deklaracja typu zmiennej i inicjalizacja wartosci */
+        char wybor; /* okreslenie wartosci stalej wybor jako "*" */
+
+        scanf("%c", &wybor); /* czytaj z konsoli i zapisz pod wybor */
+
+        /* if (wyrazenie) {blok jezeli wyrazenie prawdziwe}
+           else {blok jezeli wyrazenie galszywe}; */
+
+        if (wybor == '*')
+        { /* sprawdzana czy wybor jest * to nie jest '=' tylko '==' */
+                printf("Wynik mnozenia: %i\n", mnoz(a, b));
+
+        }else{
+
+                printf("wynik dodawania: %i\n", dodaj (a, b));
+        };              /* ten srednik jest opcjonalny jezeli jest klamra */
+
         return 0;
 }
